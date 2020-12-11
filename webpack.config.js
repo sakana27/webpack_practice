bb94fb02972777f1dac0fd186721c6abcde3d994
harada_27 <43932450+sakana27@users.noreply.github.com>
2020-12-11 14:18:10 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,20 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,//出力されるhtmlコードを1行にまとめない
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -46,7 +60,12 @@ module.exports = {
       filename: './stylesheets/main.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
+      template: './src/templates/index.pug',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html'
     }),
     new CleanWebpackPlugin(),
   ]
